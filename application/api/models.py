f52a80b4d249522af7a3f4798ef4d457236d50d8
mansehajsingh <msehajs@gmail.com>
2022-01-29 22:21:20 -0500

    Created User model.

--- application/api/models.py
+++ application/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(primary_key=True, unique=True, max_length=16)
+    full_name = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
